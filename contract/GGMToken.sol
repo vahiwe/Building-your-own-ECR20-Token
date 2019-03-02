d3aab86fdcc35e4e62c09cd6c62d3a2ecaad641e
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2019-03-02 19:52:22 +0100

    Add comments for clarification

--- contract/GGMToken.sol
+++ contract/GGMToken.sol
@@ -1,6 +1,23 @@
 pragma solidity ^0.5.00;
 
+// ----------------------------------------------------------------------------
+// 'GGMToken' token contract
+//
+// Deployed to : 0xFEB02D9383C49A8373F88e82EbCecB553c1837bf
+// Symbol      : GGM
+// Name        : GGMToken
+// Total supply: 100000000
+// Decimals    : 18
+//
+// Enjoy.
+//
+// (c) by Ahiwe Onyebuchi Valentine.
+// ----------------------------------------------------------------------------
 
+
+// ----------------------------------------------------------------------------
+// Safe maths
+// ----------------------------------------------------------------------------
 contract SafeMath {
     function safeAdd(uint a, uint b) public pure returns (uint c) {
         c = a + b;
@@ -21,6 +38,10 @@ contract SafeMath {
 }
 
 
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
 contract ERC20Interface {
     function totalSupply() public view returns (uint);
     function balanceOf(address tokenOwner) public view returns (uint balance);
@@ -34,11 +55,19 @@ contract ERC20Interface {
 }
 
 
+// ----------------------------------------------------------------------------
+// Contract function to receive approval and execute function in one call
+//
+// Borrowed from MiniMeToken
+// ----------------------------------------------------------------------------
 contract ApproveAndCallFallBack {
     function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public;
 }
 
 
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
 contract Owned {
     address public owner;
     address public newOwner;
@@ -66,7 +95,10 @@ contract Owned {
 }
 
 
-
+// ----------------------------------------------------------------------------
+// ERC20 Token, with the addition of symbol, name and decimals and assisted
+// token transfers
+// ----------------------------------------------------------------------------
 contract GGMToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
@@ -77,6 +109,9 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     mapping(address => mapping(address => uint)) allowed;
 
 
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
     constructor() public {
         symbol = "GGM";
         name = "GGMToken";
@@ -87,17 +122,27 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     }
 
 
+    // ------------------------------------------------------------------------
+    // Total supply
+    // ------------------------------------------------------------------------
     function totalSupply() public view returns (uint) {
         return _totalSupply - balances[address(0)];
     }
 
 
+    // ------------------------------------------------------------------------
+    // Get the token balance for account tokenOwner
+    // ------------------------------------------------------------------------
     function balanceOf(address tokenOwner) public view returns (uint balance) {
         return balances[tokenOwner];
     }
 
 
-
+    // ------------------------------------------------------------------------
+    // Transfer the balance from token owner's account to to account
+    // - Owner's account must have sufficient balance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
     function transfer(address to, uint tokens) public returns (bool success) {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
@@ -105,6 +150,15 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
         return true;
     }
 
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account
+    //
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+    // recommends that there are no checks for the approval double-spend attack
+    // as this should be implemented in user interfaces 
+    // ------------------------------------------------------------------------
     function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
@@ -112,6 +166,15 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     }
 
 
+    // ------------------------------------------------------------------------
+    // Transfer tokens from the from account to the to account
+    // 
+    // The calling account must already have sufficient tokens approve(...)-d
+    // for spending from the from account and
+    // - From account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
@@ -121,12 +184,20 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     }
 
 
-
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
     function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
 
 
+    // ------------------------------------------------------------------------
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account. The spender contract function
+    // receiveApproval(...) is then executed
+    // ------------------------------------------------------------------------
     function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
@@ -135,12 +206,17 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     }
 
 
-
+    // ------------------------------------------------------------------------
+    // Don't accept ETH
+    // ------------------------------------------------------------------------
     function () external payable {
         revert();
     }
 
 
+    // ------------------------------------------------------------------------
+    // Owner can transfer out any accidentally sent ERC20 tokens
+    // ------------------------------------------------------------------------
     function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
         return ERC20Interface(tokenAddress).transfer(owner, tokens);
     }
