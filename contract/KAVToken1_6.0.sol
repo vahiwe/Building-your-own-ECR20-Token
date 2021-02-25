14e5f245e119dd727c7c922eb9fce5bdebad38fd
Aleksey7477 <79647315+Aleksey7477@users.noreply.github.com>
2021-02-25 21:40:03 +0700

    Update and rename GGMToken_6.0.sol to KAVToken1_6.0.sol

--- contract/KAVToken1_6.0.sol
+++ contract/KAVToken1_6.0.sol
@@ -1,11 +1,11 @@
 pragma solidity 0.6.6;
 
 // ----------------------------------------------------------------------------
-// 'GGMToken' token contract
+// 'KAVToken1' token contract
 //
-// Deployed to : 0xFEB02D9383C49A8373F88e82EbCecB553c1837bf
-// Symbol      : GGM
-// Name        : GGMToken
+// Deployed to : 0x90f9841f4245C50FdCf07f1801c887A2e918d1a2
+// Symbol      : KAVT
+// Name        : KAVToken1
 // Total supply: 100000000
 // Decimals    : 18
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract GGMToken is ERC20Interface, Owned, SafeMath {
+contract KAVToken1 is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "CCM";
-        name = "CCMToken";
+        symbol = "KAVT";
+        name = "KAVToken1";
         decimals = 0;
         _totalSupply = 100000000;
-        balances[0x4B6408E5De3D348C0cd571E67Aa2dDBDe563AD58] = _totalSupply;
-        emit Transfer(address(0), 0x4B6408E5De3D348C0cd571E67Aa2dDBDe563AD58, _totalSupply);
+        balances[0x90f9841f4245C50FdCf07f1801c887A2e918d1a2] = _totalSupply;
+        emit Transfer(address(0), 0x90f9841f4245C50FdCf07f1801c887A2e918d1a2, _totalSupply);
     }
 
 
